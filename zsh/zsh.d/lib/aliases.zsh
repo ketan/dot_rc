6a9dae3bbc86df429639c9875f4c38591117ede0
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2012-08-08 12:57:05 +0530

    Use mysql aliases based on mysql_select

--- zsh/zsh.d/lib/aliases.zsh
+++ zsh/zsh.d/lib/aliases.zsh
@@ -12,8 +12,8 @@ alias -g ...='../..'
 alias -g ....='../../..'
 alias -g .....='../../../..'
 
-alias mysqlstart='sudo /opt/local/bin/mysqld_safe5 &'
-alias mysqlstop='/opt/local/bin/mysqladmin5 -u root -p shutdown'
+alias mysqlstart='sudo mysqld_safe &'
+alias mysqlstop='mysqladmin -u root -p shutdown'
 
 alias reload="source ~/.zshrc"
 
