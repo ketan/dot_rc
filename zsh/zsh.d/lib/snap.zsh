bdd963c2fabbb13c6b4629fd38d30774d64b76d4
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-10-15 13:40:04 +0530

    Some refactoring for vagrant

--- zsh/zsh.d/lib/snap.zsh
+++ zsh/zsh.d/lib/snap.zsh
@@ -3,8 +3,30 @@ if [ -z "$PS1" ]; then
   return
 fi
 
-export PATH=$PATH:$HOME/projects/snap/go-saas-chef/scripts
+case $(uname -o) in
+  Darwin)
+    export PATH=$PATH:$HOME/projects/snap/go-saas-chef/scripts
 
-fpath=($HOME/projects/snap/go-saas-chef/scripts/completion $fpath)
+    fpath=($HOME/projects/snap/go-saas-chef/scripts/completion $fpath)
+    autoload -Uz compinit && compinit
 
-autoload -Uz compinit && compinit
+  ;;
+  GNU/Linux)
+    export JAVA_HOME=/opt/local/java/1.8
+    export PATH=$JAVA_HOME/bin:$PATH
+
+    export PATH=/opt/local/ruby/2.0.0-p353/bin:$PATH
+    export PATH=/opt/local/python/2.7.5/bin:$PATH
+    export PATH=/opt/local/nodejs/0.10.28/bin:$PATH
+    export PATH=/opt/local/awscli/bin:$PATH
+
+    export PATH=/go-saas-src/go-saas-chef/scripts:$PATH
+
+    export EDITOR=${EDITOR:-vim}
+    export BUNDLE_JOBS=4
+
+    fpath=(/go-saas-src/go-saas-chef/scripts/completion $fpath)
+    autoload -Uz compinit && compinit
+
+  ;;
+esac
