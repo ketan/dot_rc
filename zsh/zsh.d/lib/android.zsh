82d6c6d276eedc53ba94ea22eb14bd7ec559c0aa
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-02-12 10:10:18 +0530

    Some improvements

--- zsh/zsh.d/lib/android.zsh
+++ zsh/zsh.d/lib/android.zsh
@@ -1,4 +1,4 @@
-if [ -d "$HOME/projects/mobile/adt-bundle-mac-x86_64-20130219/sdk" ]; then
-  export ANDROID_HOME=$HOME/projects/mobile/adt-bundle-mac-x86_64-20130219/sdk
+if [ -d "$HOME/local/android-sdk-macosx" ]; then
+  export ANDROID_HOME=$HOME/local/android-sdk-macosx
   export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
 fi
--- zsh/zsh.d/lib/rails.zsh
+++ zsh/zsh.d/lib/rails.zsh
@@ -8,4 +8,7 @@ export RUBY_HEAP_MIN_SLOTS=4000000
 export RUBY_GC_HEAP_FREE_SLOTS=$RUBY_FREE_MIN
 export RUBY_GC_HEAP_INIT_SLOTS=$RUBY_HEAP_MIN_SLOTS
 
-export JRUBY_OPTS='-J-XX:+TieredCompilation -J-XX:TieredStopAtLevel=1 -X-C -Xcompile.invokedynamic=false'
+export JRUBY_OPTS='--dev -J-Xmx1g'
+
+# to prevent nokogiri to compile libxml/libxml and the works
+export NOKOGIRI_USE_SYSTEM_LIBRARIES=1
